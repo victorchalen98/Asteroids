d1d4bac120bb777735771e1eb82854fa95a5fd8b
victorchalen98 <57729512+victorchalen98@users.noreply.github.com>
2025-10-30 11:05:22 -0500

    Adding asteroids

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 from circleshape import *
 from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -12,9 +14,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
+    # --- GRUPOS ---
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
+    asteroid_field = AsteroidField()
 
     running = True
     while running:
@@ -22,12 +34,14 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        dt = clock.tick(60) / 1000  # Delta time (por si luego lo usas)
+        dt = clock.tick(60) / 1000  # Delta time
+
+        updatable.update(dt)
 
         screen.fill("black")
-        player.draw(screen)
+        for sprite in drawable:
+            sprite.draw(screen)
         pygame.display.flip()
-        player.update(dt)
 
     pygame.quit()
 
